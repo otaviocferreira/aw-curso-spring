0ff0c72df2c2e63defe5bf8cfd80d1344a7aa58a
Otavio Ferreira <otavio.cferreira@gmail.com>
2017-02-21 23:35:58 -0300

    Final da aula 2.5

--- src/main/java/com/algaworks/titulos/controller/TituloController.java
+++ src/main/java/com/algaworks/titulos/controller/TituloController.java
@@ -1,15 +1,21 @@
 package com.algaworks.titulos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.titulos.model.Titulo;
+import com.algaworks.titulos.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	@Autowired
+	public Titulos titulos;
 
 	@GetMapping("/novo")
 	public String novo() {
@@ -17,8 +23,10 @@ public class TituloController {
 	}
 	
 	@PostMapping("/novo")
-	public String salvar(Titulo titulo) {
-		System.out.println(">>>" + titulo.getDescricao());
-		return "cadastro-titulo";
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("cadastro-titulo");
+		mv.addObject("mensagem", "Título salvo com sucesso!");
+		return mv;
 	}
 }
