3b7999c9c8eed9b6a432122a443c1164ca78e30a
Otavio Ferreira <otavio.cferreira@gmail.com>
2017-03-05 17:34:10 -0300

    Validacoes com Bean Validation - Aula 2.13

--- src/main/java/com/algaworks/titulos/controller/TituloController.java
+++ src/main/java/com/algaworks/titulos/controller/TituloController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,13 +27,20 @@ public class TituloController {
 	@GetMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cadastro-titulo");
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@PostMapping("/novo")
-	public ModelAndView salvar(Titulo titulo) {
-		titulos.save(titulo);
+	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
 		ModelAndView mv = new ModelAndView("cadastro-titulo");
+		
+		if (errors.hasErrors()) {
+			return mv;
+		}
+		
+		titulos.save(titulo);
+		
 		mv.addObject("mensagem", "Título salvo com sucesso!");
 		return mv;
 	}
