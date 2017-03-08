a2652acff27a3f4a25a52c1e9e75c31554bf7421
Otavio Ferreira <otavio.cferreira@gmail.com>
2017-03-08 02:09:09 -0300

    Plugin para calendario - Aula 3.2

--- src/main/java/com/algaworks/titulos/controller/TituloController.java
+++ src/main/java/com/algaworks/titulos/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -42,10 +43,15 @@ public class TituloController {
 			return CADASTRO_VIEW;
 		}
 		
+		try  {
 		titulos.save(titulo);
 		
 		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
 		return "redirect:/titulos/novo";
+		} catch(DataIntegrityViolationException ex) {
+			errors.rejectValue("dataVencimento", null, "Formato de data inválido");
+			return CADASTRO_VIEW;
+		}
 	}
 	
 	@RequestMapping("{codigo}")
